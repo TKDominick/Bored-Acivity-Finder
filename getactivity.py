86802d4d2f8968bf054d08c39b260c38c6430051
Dominick Long <domaidlong@gmail.com>
2019-06-18 12:01:03 -0500

    Structured .py file

--- getactivity.py
+++ getactivity.py
@@ -1,1 +1,58 @@
+import requests
+import time
+import pprint
+
+dict = {}
+while True:
+	activity_by= input("Get an Activity: [R]andom, by [T]ype, by number of [P]articipants, by Price [R]ange, by [A]cessibility: ")
+	if activity_by == 'R':
+		resp = requests.get('http://www.boredapi.com/api/activity/')
+		dict = resp.json()
+		break
+		
+	elif activity_by == 'T':
+		resp = requests.get('http://www.boredapi.com/api/activity/')
+		dict = resp.json()
+		
+		break
+		
+	elif activity_by ==  'P':
+		resp = requests.get('http://www.boredapi.com/api/activity/')
+		dict = resp.json()
+		
+		break
+		
+	elif activity_by == 'R':
+		resp = requests.get('http://www.boredapi.com/api/activity/')
+		dict = resp.json()
+		
+		break
+		
+	elif activity_by == 'A':
+		acessibilitymax = input("Input an accessibility max between 0 and 1, with 0 being the lowest: ")
+		resp = requests.get('http://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility='+acessibilitymax)
+		dict = resp.json()
+		
+		break
+		
+	else:
+		print("Please enter a valid input: ")
+		continue
+
+print("════════════════════════════════════════════════════════════════")
+print("")
+print(dict["activity"])
+print("Type: " + dict["type"])
+print("Participants: " + str(dict["participants"]))
+print("Price: " + str(dict["price"]))
+print("Accessibility: " + str(dict["accessibility"]))
+print("")
+print("════════════════════════════════════════════════════════════════")
+
+
+
+
+
+
+
 
