f72393defd732093df1a3b3353ca61b7900547ce
Dominick Long <domaidlong@gmail.com>
2019-06-18 13:16:09 -0500

    Completed basic structure and options

--- getactivity.py
+++ getactivity.py
@@ -1,53 +1,64 @@
 import requests
-import time
-import pprint
 
 dict = {}
 while True:
-	activity_by= input("Get an Activity: [R]andom, by [T]ype, by number of [P]articipants, by Price [R]ange, by [A]cessibility: ")
-	if activity_by == 'R':
-		resp = requests.get('http://www.boredapi.com/api/activity/')
-		dict = resp.json()
-		break
-		
-	elif activity_by == 'T':
-		resp = requests.get('http://www.boredapi.com/api/activity/')
-		dict = resp.json()
-		
-		break
-		
-	elif activity_by ==  'P':
-		resp = requests.get('http://www.boredapi.com/api/activity/')
-		dict = resp.json()
-		
-		break
-		
-	elif activity_by == 'R':
-		resp = requests.get('http://www.boredapi.com/api/activity/')
-		dict = resp.json()
-		
-		break
-		
-	elif activity_by == 'A':
-		acessibilitymax = input("Input an accessibility max between 0 and 1, with 0 being the lowest: ")
-		resp = requests.get('http://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility='+acessibilitymax)
-		dict = resp.json()
-		
-		break
-		
-	else:
-		print("Please enter a valid input: ")
+	while True:
+		activity_by= input("Get an Activity: [R]andom, by [T]ype, by number of [P]articipants, by [Pr]ice Range, by [A]cessibility: ")
+		if activity_by == 'R' or activity_by == 'r':
+			resp = requests.get('http://www.boredapi.com/api/activity/')
+			dict = resp.json()
+			break
+			
+		elif activity_by == 'T' or activity_by == 't':
+			acttype = input("Input activity type (education, recreational, social, diy, charity, cooking, relaxation, music, busywork): ")
+			resp = requests.get('http://www.boredapi.com/api/activity?type='+acttype)
+			dict = resp.json()
+			
+			break
+			
+		elif activity_by ==  'P' or activity_by == 'p':
+			parts = input("Input number of participants: ")
+			resp = requests.get('http://www.boredapi.com/api/activity?participants='+parts)
+			dict = resp.json()
+			
+			break
+			
+		elif activity_by == 'Pr' or activity_by == 'pr':
+			maxprice = input("Input a max price, with 0 being the lowest: ")
+			resp = requests.get('http://www.boredapi.com/api/activity?minprice=0&maxprice='+maxprice)
+			dict = resp.json()
+			
+			break
+			
+		elif activity_by == 'A' or activity_by == 'a':
+			acessibilitymax = input("Input a max accessibility between 0 and 1, with 0 being the lowest: ")
+			resp = requests.get('http://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility='+acessibilitymax)
+			dict = resp.json()
+			
+			break
+			
+		else:
+			print("Please enter a valid input: ")
+			continue
+	
+	print("════════════════════════════════════════════════════════════════")
+	print("")
+	print(dict["activity"])
+	print("Type: " + dict["type"])
+	print("Participants: " + str(dict["participants"]))
+	print("Price: " + str(dict["price"]))
+	print("Accessibility: " + str(dict["accessibility"]))
+	print("")
+	print("════════════════════════════════════════════════════════════════")
+	
+	rerun = input("[R}erun or [Q]uit")
+	if rerun == 'R' or rerun == 'r':
 		continue
+	elif rerun == 'Q' or rerun == 'q':
+		break
+	else:
+		break
 
-print("════════════════════════════════════════════════════════════════")
-print("")
-print(dict["activity"])
-print("Type: " + dict["type"])
-print("Participants: " + str(dict["participants"]))
-print("Price: " + str(dict["price"]))
-print("Accessibility: " + str(dict["accessibility"]))
-print("")
-print("════════════════════════════════════════════════════════════════")
 
 
 
